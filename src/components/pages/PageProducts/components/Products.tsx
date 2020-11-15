0f8be781bdc5b9acae0ea160eee152d05312f8a9
Stanislav Tyurin <stanislav.tyurin@showpad.com>
2020-11-15 17:46:59 +0300

    Task 4 (After Lecture4 "Cloud Databases Overview Overview of DB solutions (SQL, No-SQL (Dynamodb, Mongo, Redis, Elastic Search), NewSQL) Infrastructure As Code (IaC), deployment sample to deploy RDS")

--- src/components/pages/PageProducts/components/Products.tsx
+++ src/components/pages/PageProducts/components/Products.tsx
@@ -47,7 +47,7 @@ export default function Products() {
           <Card className={classes.card}>
             <CardMedia
               className={classes.cardMedia}
-              image={product.imageUrl}
+              image={product.imageurl}
               title="Image title"
             />
             <CardContent className={classes.cardContent}>
