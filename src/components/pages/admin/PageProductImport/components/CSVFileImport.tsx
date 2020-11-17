2994a544c5ce3f7666765ab6df11eff753158b4e
Stanislav Tyurin <stanislav.tyurin@showpad.com>
2020-11-17 22:00:29 +0300

    fix: fix csv content type on windows env

--- src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
+++ src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
@@ -43,7 +43,10 @@ export default function CSVFileImport({url, title}: CSVFileImportProps) {
       console.log('Uploading to: ', response.data.url)
       const result = await fetch(response.data.url, {
         method: 'PUT',
-        body: file
+        body: file,
+        headers: {
+          'Content-Type': 'text/csv'
+        }
       })
       console.log('Result: ', result)
       setFile('');
